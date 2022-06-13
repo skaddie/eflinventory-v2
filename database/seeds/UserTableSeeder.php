bd8d6290d2aa04d226f36e0d55fb0347d2aa6d9e
skaddie <87253680+skaddie@users.noreply.github.com>
2022-06-13 19:21:41 +0800

    update

---
+++ database/seeds/UserTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use App\Models\Role;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UserTableSeeder extends Seeder {
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run() {
+        $role_manager = Role::where('name', 'Manager')->first();
+
+        $manager = new User();
+        $manager->name = 'System Administrator';
+        $manager->username = 'admin';
+        $manager->password = bcrypt('secret');
+        $manager->save();
+        $manager->roles()->attach($role_manager);
+    }
+}
--- web/readme.md
+++
@@ -1,2 +0,0 @@
-## Web App
-The ``Laravel`` application. You can strip the other folders off and leave only the contents within this directory if you intend to use this application on the web.
