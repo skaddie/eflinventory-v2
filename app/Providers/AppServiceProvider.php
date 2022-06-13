ddd9ac15929beaf80f3d0a8eaad51f6067cd5064
skaddie <87253680+skaddie@users.noreply.github.com>
2022-06-14 00:59:46 +0800

    update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -12,6 +12,10 @@ class AppServiceProvider extends ServiceProvider {
      * @return void
      */
     public function boot() {
+        if(env('REDIRECT_HTTPS'))
+        {
+            \URL::forceScheme('https');
+        }
 
     }
 
