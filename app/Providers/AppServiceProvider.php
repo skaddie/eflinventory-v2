c73bb69c4b4dd37e9bcedd706246351db158455f
skaddie <87253680+skaddie@users.noreply.github.com>
2022-06-13 22:46:05 +0800

    development

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -12,9 +12,12 @@ class AppServiceProvider extends ServiceProvider {
      * @return void
      */
     public function boot() {
-        If (env('APP_ENV') !== 'local') {
-            $this->app['request']->server->set('HTTPS', true);
+        if(env('REDIRECT_HTTPS'))
+        {
+            \URL::forceScheme('https');
         }
+
+        Schema::defaultStringLength(191);
     }
 
     /**
