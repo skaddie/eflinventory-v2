f26cd140695ccd3e9877e0556fbd9fd743ecd4dc
skaddie <87253680+skaddie@users.noreply.github.com>
2022-06-14 11:58:54 +0800

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,24 +11,24 @@ class AppServiceProvider extends ServiceProvider {
      *
      * @return void
      */
-    public function boot()
-    {
-        if ($this->isSecure()) {
-            \URL::forceSchema('https');
-        }
+public function boot()
+{
+    if ($this->isSecure()) {
+        \URL::forceScheme('https');
     }
-    
-    public function isSecure()
-    {
-        $isSecure = false;
-        if (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') {
-            $isSecure = true;
-        } elseif (!empty($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https' || !empty($_SERVER['HTTP_X_FORWARDED_SSL']) && $_SERVER['HTTP_X_FORWARDED_SSL'] == 'on') {
-            $isSecure = true;
-        }
-    
-        return $isSecure;
+}
+
+public function isSecure()
+{
+    $isSecure = false;
+    if (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') {
+        $isSecure = true;
+    } elseif (!empty($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https' || !empty($_SERVER['HTTP_X_FORWARDED_SSL']) && $_SERVER['HTTP_X_FORWARDED_SSL'] == 'on') {
+        $isSecure = true;
     }
+
+    return $isSecure;
+}
 
     /**
      * Register any application services.
